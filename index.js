214d06fcdfe5846e21ff02ededf72bd5088698e3
Oladipupo <dipoakanji57@gmail.com>
2024-12-18 11:36:51 +0100

    Added more typescript form validation

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 var usernameInputJ = document.getElementById('username');
+var useremailInputJ = document.getElementById('useremail');
+var userpasswordInputJ = document.getElementById('userpassword');
 var resultJ = document.getElementById('result');
+var result1J = document.getElementById('result1');
+var result2J = document.getElementById('result2');
 var btnJ = document.getElementById("submitBtn");
 var usernameJ;
+var useremailJ;
+var userpasswordJ;
 btnJ.addEventListener('click', function () {
     usernameJ = usernameInputJ.value;
-    if (usernameJ) {
-        resultJ.textContent = 'Entered Username: ' + usernameJ;
+    useremailJ = useremailInputJ.value;
+    userpasswordJ = userpasswordInputJ.value;
+    if (usernameJ === "" || useremailJ === "" || userpasswordJ === "") {
+        resultJ.innerHTML = "Name email and password are required";
     }
     else {
-        resultJ.textContent = 'Input field can not be empty';
+        resultJ.innerHTML = "Name: " + usernameJ;
+        result1J.innerHTML = "Email: " + useremailJ;
+        result2J.innerHTML = "Password: " + userpasswordJ;
     }
 });
--- index.ts
+++ index.ts
@@ -1,13 +1,23 @@
 let usernameInput: HTMLInputElement = document.getElementById('username') as HTMLInputElement;
+let useremailInput: HTMLInputElement = document.getElementById('useremail') as HTMLInputElement
+let userpasswordInput: HTMLInputElement = document.getElementById('userpassword') as HTMLInputElement
 let result: HTMLParagraphElement = document.getElementById('result') as HTMLParagraphElement;
+let result1: HTMLParagraphElement = document.getElementById('result1') as HTMLParagraphElement
+let result2: HTMLParagraphElement = document.getElementById('result2') as HTMLParagraphElement
 let btn: HTMLButtonElement = document.getElementById("submitBtn") as HTMLButtonElement
 let username: string;
+let useremail: string;
+let userpassword: string;
 
 btn.addEventListener('click', function () {
   username = usernameInput.value;
-  if (username) {
-    result.textContent ='Entered Username: ' + username;
+  useremail = useremailInput.value;
+  userpassword = userpasswordInput.value;
+  if (username === "" || useremail === "" || userpassword === "") {
+    result.innerHTML = "Name email and password are required"
   } else {
-    result.textContent ='Input field can not be empty';
+    result.innerHTML = "Name: " + username
+    result1.innerHTML = "Email: " + useremail
+    result2.innerHTML = "Password: " + userpassword
   }
 });
